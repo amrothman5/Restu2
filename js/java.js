60fd12bfdcbd43bba8c02300df81a94f8a5a4b46
Amrothman5 <Amrothman5@gmail.com>
2021-12-12 19:18:26 +0200

    first commit

---
+++ js/java.js
@@ -0,0 +1,12 @@
+function star(){
+    let star1 = document.getElementsByClassName("fas");
+    // star1.classlist.add("fas1");
+    star1.style.opacity="0px";
+}
+
+// =====================================================================
+
+function myFunction() {
+    var x = document.URL;
+    document.getElementById("jinu").src = 'https://www.facebook.com/plugins/share_button.php?href='+x+'&layout=button_count&size=large&mobile_iframe=true&width=83&height=28&appId';
+}
